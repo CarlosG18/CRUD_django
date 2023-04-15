37e543f4c04c37dabe058bca1873b89348141cff
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-04-14 23:25:11 -0300

    add authentic user part1

--- system/crud/urls.py
+++ system/crud/urls.py
@@ -5,7 +5,7 @@ app_name='crud'
 
 urlpatterns = [
   path('', views.index, name="index"),
-  path('saveuser/', views.saveuser, name="saveuser"),
+ # path('saveuser/', views.saveuser, name="saveuser"),
   path("registrer/", views.registrer, name="registrer"),
   path("save_cadastro/", views.save_cadastro, name="save_cadastro"),
   path("check_user/", views.check_user, name="check_user"),
--- system/crud/views.py
+++ system/crud/views.py
@@ -1,70 +1,60 @@
-import email
+
 from django.shortcuts import render
 # from .models import User
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 
-
 def index(request):
   return render(request, 'crud/Index.html')
 
-def saveuser(request):
-  email = request.POST["email"]
-  senha = request.POST["password"]
-  return render(request, 'crud/home.html', 
-  {
-    "email": email,
-    "senha": senha
-  })
-
 def registrer(request):
-  return render(request, 'crud/registrer.html')
+  if request.method == "GET":
+    return render(request, 'crud/registrer.html', {"error_username": None, "error_email": None})
 
 def save_cadastro(request):
-  nome = request.POST["nome"]
-  # idade = request.POST["idade"]
-  email = request.POST["email"]
-  senha = request.POST["password"]
-  
-  user = User.objects.create_user(nome, senha, email)
-
-  # user = User(name=nome,age=idade,email=email,password=senha)
-  user.save()
-  aproved = 1
-  return render(request, 'crud/Index.html',{
-    "aproved": aproved
-  })
+  if request.method == "GET":
+    return render(request, 'crud/registrer.html')
+  else:
+    nome = request.POST["nome"]
+    email = request.POST["email"]
+    senha = request.POST["password"]
+    
+    test_username = User.objects.filter(username=nome)
+    test_email = User.objects.filter(email=email)
+    
+    if test_username:
+      return render(request, 'crud/registrer.html', {
+        "error_username": "username não disponível!"
+      })
+    elif test_email:
+      return render(request, 'crud/registrer.html', {
+        "error_email": "email já cadastrado!"
+      })
+    else:
+      user = User.objects.create_user(username=nome, email=email, password=senha)
+      user.save()
+      return render(request, 'crud/Index.html',{
+      "aproved": "usuário cadastrado com sucesso!"
+      })
   
 def check_user(request):
-  email_check = request.POST["email"]
-  senha_check = request.POST["password"]
-  
-  user = authenticate(email=email_check, password=senha_check)
-
-  if user is not None:
-    return HttpResponseRedirect(reverse('crud:home'))
+  if request.method == "GET":
+    return render(request, 'crud/index.html', {"error": None})
   else:
-    error = {
-    "email": email_check,
-    "error": "usuário não cadastrado"
-  }
-  return render(request, 'crud/Index.html', error)
-  
-  # users = User.objects.all()
-  
-  # for user in users:
-  #   if email_check == user.email and senha_check == user.password:
-  #     # request.session['user'] = user
-  #     return HttpResponseRedirect(reverse('crud:home'))
-  #     # return render(request, 'crud/home.html', {"user":user, "users": users})
-  # error = {
-  #   "email": email_check,
-  #   "error": "usuário não cadastrado"
-  # }
-  # return render(request, 'crud/index.html', error)
+    username = request.POST["nome"]
+    senha_check = request.POST["password"]
+    user = authenticate(username=username, password=senha_check)
+
+    if user is not None:
+      return HttpResponseRedirect(reverse('crud:home'))
+    else:
+      error = {
+        "error": "usuário ou senha invalidos"
+      }
+      return render(request, 'crud/Index.html', error)
 
 def home(request):
   return render(request, 'crud/home.html')
